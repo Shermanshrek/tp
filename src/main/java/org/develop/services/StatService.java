e60d38d29abe12571700910eae52cd9fcfe98325
razdryzgULTIMATE <alphanomore2222@gmail.com>
2024-12-21 23:38:27 +0300

    nfhgfпрпар

--- src/main/java/org/develop/services/StatService.java
+++ src/main/java/org/develop/services/StatService.java
@@ -8,7 +8,9 @@ import org.develop.repository.StatRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,7 +26,13 @@ public class StatService {
     }
 
     public void saveStat(StatDTO dto){
+        System.out.println(dto);
         StatModel model = new StatModel();
+        // Определяем формат даты
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        // Преобразуем строку в LocalDate
+        LocalDate localDate = LocalDate.parse(dto.getDate(), formatter);
         model.setExerciseDate(LocalDateTime.parse(dto.getDate()));
         model.setUsername(dto.getUsername());
         model.setErrorCount(dto.getErrors());
